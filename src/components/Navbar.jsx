43b50aa76755e3b13d1c6ee12c3b8d4462441bbb
Riyakonde <riyakonde224@gmail.com>
2024-12-29 14:26:27 +0530

    new additions

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ import { ShopContext } from '../context/ShopContext'
 const Navbar = () => {
     const[visible,setVisible]=useState(false)
 
-    const{setShowSearch}=useContext(ShopContext);
+    const{setShowSearch,getCartCount}=useContext(ShopContext);
 
 
   return (
@@ -53,7 +53,7 @@ const Navbar = () => {
         <div className='flex items-center gap-6'>
             <img onClick={()=>setShowSearch(true)} src={assets.search_icon} className='w-5 cursor-point' alt=""></img>
             <div className='group realtive'>
-                <img src={assets.profile_icon} className='w-5 cursor-pointer' alt=""/>
+                <Link to='/login'><img src={assets.profile_icon} className='w-5 cursor-pointer' alt=""/></Link>
                 <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
                     <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
                         <p className='cursor-pointer hover:text-black'>My Profile</p>
@@ -68,7 +68,7 @@ const Navbar = () => {
             <Link to='/cart' className='relative'>
                 <img src={assets.cart_icon} className='w-5 min-w-5' alt=""></img>
                 <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black aspect-square rounded-full text-[8px] text-white'> 
-                    10</p>
+                    {getCartCount()}</p>
                 </Link>
 
             <img onClick={()=>setVisible(true)} src={assets.menu_icon} className='w-5 cursor-pointer sm:hidden'></img>
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -2,12 +2,14 @@ import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { ShopContext } from '../context/ShopContext';
 import { assets } from '../assets/assets';
+import RelatedProducts from '../components/RelatedProducts';
 
 const Product = () => {
   const {productId}=useParams();
-  const {products,currency}=useContext(ShopContext);
+  const {products,currency,addToCart}=useContext(ShopContext);
   const[productData, setProductData]=useState(false)
   const [image,setImage]=useState('')
+  const[size,setsize]=useState('')
 
 const fetchProductData=async() =>{
 
@@ -16,7 +18,7 @@ const fetchProductData=async() =>{
       setProductData(item)
       setImage(item.image[0])
 
-      console.log(item);
+      //console.log(item);
       return null;
 
     }
@@ -67,13 +69,34 @@ fetchProductData();
             <p>Select Size</p>
             <div className='flex gap-2'>
               {productData.sizes.map((item,index)=>(
-                <button key={index}>{item}</button>
+              <button onClick={()=>setsize(item)} className={`border py-2 px-4 bg-gray-100 ${item===size ?'border-orange-500':''}`} key={index}>{item}</button>
               ))}
             </div>
 
           </div>
+          <button onClick={()=>addToCart(productData._id,size)} className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700 '>ADD TO CART</button>
+          <hr className='mt-8 sm:w-4/5'/>
+          <div className='text-sm text-gray-500 mt-5 flex flex-col gap-1'>
+            <p>100% original product</p>
+            <p>Cash on delivery available on this product</p>
+            <p>Easy return & exchange</p>
+            </div>  
          </div>
       </div>
+      {/*description */}
+      <div className='mt-20'>
+        <div className='flex'>
+          <b className='border px-5 py-3 text-sm'>Description</b>
+          <p className='border px-5 py-3 text-sm'>Review (122) </p>
+        </div>
+        <div className='flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500'>
+          <p>Shop the hottest trends in clothing, footwear, and accessories for men, women, and kids.
+          </p>
+          <p>Effortlessly blend style and comfort with the Classic Comfort T-Shirt. Perfect for every occasion, this wardrobe essential is designed to keep you looking sharp and feeling great all day long.</p>
+        </div>
+      </div>
+      {/*related product */}
+      <RelatedProducts category={productData.category } subCategory={productData.subCategory}/>
        </div>
   ) : <div className='opacity-0'>
 
