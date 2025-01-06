59e4ba624f3c53faa7d2ce169e453fc77a7effcc
Riyakonde <riyakonde224@gmail.com>
2025-01-06 21:53:19 +0530

    new commit

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -46,7 +46,8 @@ const registerUser = async (req, res) => {
       return res.json({
         success: false,
         message: "please enter  strong password",
-      });
+      }
+    );
     }
 
     //hashing user password
@@ -56,14 +57,14 @@ const registerUser = async (req, res) => {
     const newUser = new userModel({
       name,
       email,
-      password: hashedPassword,
+      password:hashedPassword
     });
 
-    const user = await newUser.save();
+    const user = await newUser.save() 
 
-    const token = createToken(user._id);
+    const token = createToken(user._id)
 
-    res.json({ success: true, token });
+    res.json({ success: true, token })
   } catch (error) {
     console.log(error);
     res.json({ success: false, message: error.message });
--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -7,11 +7,11 @@ const productSchema = new mongoose.Schema({
   image: { type: Array, required: true },
   category: { type: String, required: true },
   subCategory: { type: String, required: true },
-  size: { type: Array, required: true },
+  sizes: { type: Array, required: true },
   bestSeller: { type: Boolean },
-  date: { type: Number, required },
+  date: { type: Number, required:true },
 });
 
-const productModel=mongoose.model.product||mongoose.model("product",productSchema);
+const productModel=mongoose.models.product||mongoose.model("product",productSchema);
 
 export default productModel
